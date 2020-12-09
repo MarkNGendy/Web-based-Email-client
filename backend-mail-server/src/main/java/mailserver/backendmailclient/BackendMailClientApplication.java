be3240b07c8841b84b41ca404375931bf83ee72f
MarkNGendy <markngendy@gmail.com>
2020-12-09 21:14:06 +0200

    Adding project structure

---
+++ backend-mail-server/src/main/java/mailserver/backendmailclient/BackendMailClientApplication.java
@@ -0,0 +1,13 @@
+package mailserver.backendmailclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMailClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMailClientApplication.class, args);
+	}
+
+}
---
+++ backend-mail-server/src/test/java/mailserver/backendmailclient/BackendMailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mailserver.backendmailclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
