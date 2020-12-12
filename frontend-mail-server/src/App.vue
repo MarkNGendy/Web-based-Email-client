5aaf68b9c3e78feb12517c30afa75ad5096a4a35
MarkNGendy <markngendy@gmail.com>
2020-12-12 09:06:02 +0200

    Adjusting template

--- frontend-mail-server/src/App.vue
+++ frontend-mail-server/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Login</router-link> |
+    <router-link to="/register">Register</router-link>
   </div>
   <router-view />
 </template>
@@ -27,4 +27,10 @@
     }
   }
 }
+
+@import "./assets/scss/variables.scss";
+@import "./assets/scss/form.scss";
+@import "./assets/scss/main.scss";
+@import "./assets/css/font-awesome.min.css";
+
 </style>
--- frontend-mail-server/src/router/index.js
+++ frontend-mail-server/src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Login",
+    component: Login
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/register",
+    name: "Register",
+    component: Register
   }
 ];
 
