298fd6ce5c7ebdc6db45d17c9f142a9bcfe5d6ce
mina-henen <mina-henen@users.noreply.github.com>
2020-12-19 14:07:09 +0200

    filters

---
+++ backend-mail-server/src/main/java/mailserver/backendmailclient/classes/Filter.java
@@ -0,0 +1,32 @@
+package mailserver.backendmailclient.classes;
+
+import mailserver.backendmailclient.interfaces.IFilter;
+import mailserver.backendmailclient.interfaces.IMail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter implements IFilter {
+
+    @Override
+    public List<Mail> subjectFilter(List<Mail> mails, String criteria) {
+        List<Mail> filteredList = new ArrayList<Mail>();
+        for (Mail mail : mails){
+            if(mail.getSubject().equalsIgnoreCase(criteria)){
+                filteredList.add(mail);
+            }
+        }
+        return filteredList;
+    }
+
+    @Override
+    public List<Mail> senderFilter(List<Mail> mails, String criteria) {
+        List<Mail> filteredList = new ArrayList<Mail>();
+        for (Mail mail : mails){
+            if(mail.getSender().equalsIgnoreCase(criteria)){
+                filteredList.add(mail);
+            }
+        }
+        return filteredList;
+    }
+}
--- backend-mail-server/src/main/java/mailserver/backendmailclient/interfaces/IFilter.java
+++ backend-mail-server/src/main/java/mailserver/backendmailclient/interfaces/IFilter.java
@@ -1,7 +1,12 @@
 package mailserver.backendmailclient.interfaces;
 
+import mailserver.backendmailclient.classes.Mail;
+
 import java.util.List;
 
 public interface IFilter {
-    public List<IMail> meetCriteria(List<IMail> mails);
+    public List<Mail> subjectFilter(List<Mail> mails, String criteria);
+
+    public List<Mail> senderFilter(List<Mail> mails, String criteria);
+
 }
