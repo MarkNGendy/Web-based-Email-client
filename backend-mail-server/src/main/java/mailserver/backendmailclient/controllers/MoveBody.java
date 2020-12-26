4f04eeabde3cedbba0f6b6e3bb99a836406bf57d
mina-henen <mina-henen@users.noreply.github.com>
2020-12-26 03:11:28 +0200

    move controller

---
+++ backend-mail-server/src/main/java/mailserver/backendmailclient/controllers/MoveBody.java
@@ -0,0 +1,35 @@
+package mailserver.backendmailclient.controllers;
+
+import mailserver.backendmailclient.classes.Mail;
+
+import java.util.List;
+
+public class MoveBody {
+    private List<Mail> mails;
+    private String user;
+    private String source;
+    private String target;
+
+    public MoveBody(List<Mail> mails, String user, String source, String target){
+        this.mails = mails;
+        this.user = user;
+        this.source = source;
+        this.target = target;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public List<Mail> getMails() {
+        return mails;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+}
